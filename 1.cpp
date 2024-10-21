f1b5c8a4cd76429187a3d6e91e17404285c8231c
INGEMINI <mahiagarwal.r1@gmail.com>
2024-10-22 00:22:00 +0530

    cpp files updated

--- 1.cpp
+++ 1.cpp
@@ -4,6 +4,6 @@ int num=5;
 int main(){
     int num=0;
     char c='c';
-    cout<</* hi */"hello ,this is my first cpp code  "<<::num<<endl<<c;
+    cout<</* hi */"hello ,this is my first cpp code  "<<num<<endl<<c;
     return 0;
 }
